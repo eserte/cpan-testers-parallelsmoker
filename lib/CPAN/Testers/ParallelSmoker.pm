f1f6255d92e8eea1d9867e2f99ee27634367765f
Slaven Rezic <slaven@rezic.de>
2010-04-10 11:11:50 +0200

    * wip

---
+++ lib/CPAN/Testers/ParallelSmoker.pm
@@ -0,0 +1,36 @@
+# -*- cperl -*-
+
+#
+# Author: Slaven Rezic
+#
+# Copyright (C) 2010 Slaven Rezic. All rights reserved.
+# This package is free software; you can redistribute it and/or
+# modify it under the same terms as Perl itself.
+#
+# Mail: srezic@cpan.org
+# WWW:  http://www.rezic.de/eserte/
+#
+
+package CPAN::Testers::Parallel::Smoker;
+
+use strict;
+
+=head1 NAME
+
+CPAN::Testers::ParallelSmoker
+
+=head1 AUTHOR
+
+Slaven ReziE<0x107>, E<lt>srezic@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010 by Slaven ReziE<0x107>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+=head1 SEE ALSO
+
+=cut
